6fe7f6533bd1ba8ad69a5d46833917c621035c97
Dingdong-LIU <dliuak@connect.ust.hk>
2025-01-20 01:04:05 +0800

    add: display reference

--- utils.py
+++ utils.py
@@ -98,9 +98,16 @@ def parse_patient_answer(patient_answer: list):
     # Parse the patient answer into a list of dicts, with key "Question" and "Patient Answer"
     parsed_patient_answer = []
     for i, item in enumerate(patient_answer):
-        question_id, real_question, answer = item["question_id"], item["question"], item["answer"]
+        question_id, real_question, answer, reference = item["question_id"], item["question"], item["answer"], item.get("reference", "")
         question = COPD_QUESTIONNAIRE[question_id]
-        parsed_patient_answer.append({"Question": question, "Patient Answer": answer, "Index": i+1})
+        parsed_patient_answer.append(
+            {
+                "Question": question,
+                "Patient Answer": answer,
+                "Index": i + 1,
+                "Notes": reference,
+            },
+        )
 
     # Convert the answer to pd.DataFrame with "Index" as the dataframe index
     df = pd.DataFrame(parsed_patient_answer)
